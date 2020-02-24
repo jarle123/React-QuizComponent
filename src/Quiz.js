f774c340c6f133ec5830981df143aa0f1e7b602d
Jarle <jarlef@gmail.com>
2020-02-24 14:47:11 +0100

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import QuizQuestion from "./QuizQuestion";
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -13,8 +14,14 @@ class Quiz extends Component {
     }
 
     render() {
+        let isQuizEnd = false;
+        if (this.state.quiz_position - 1 === quizData.quiz_questions.length()) {
+            isQuizEnd = true;
+        }
+
         return (
             <div>
+                {isQuizEnd && <QuizEnd></QuizEnd>}
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]  } />
             </div>
         );
---
+++ src/QuizEnd.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class QuizEnd extends Component {
+
+
+    render() {
+        return (
+
+            <div>
+                <p>Thanks for playing</p>
+                <a href={''}>Reset quiz</a>
+
+            </div>
+        );
+    }
+
+}
+
+export default QuizEnd;
